f9696ceaa674f131ef430fd47d9afc85f4bae4f7
Geoffrey McElhanon <geoffrey@mcelhanon.com>
2024-10-11 19:56:48 -0500

    Added support for specification-based criteria to be applied to partitions query (including inherited properties).

--- Application/EdFi.Ods.Api/Container/Modules/PersistenceModule.cs
+++ Application/EdFi.Ods.Api/Container/Modules/PersistenceModule.cs
@@ -352,6 +352,14 @@ namespace EdFi.Ods.Api.Container.Modules
                         }))
                 .As<IPersonUsiResolver>()
                 .SingleInstance();
+
+            builder.RegisterType<ContextualPersonUsisResolver>()
+                .As<IContextualPersonUsisResolver>()
+                .SingleInstance();
+
+            builder.RegisterType<ContextualPersonUniqueIdsResolver>()
+                .As<IContextualPersonUniqueIdsResolver>()
+                .SingleInstance();
         }
     }
 }
